3e56fe0d2be6f3a849df711cab442bc0f2369fe8
Moyrasc <noemisolac@gmail.com>
2022-11-25 18:17:15 +0100

    create routes login,recovery password and create account

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -8,4 +8,20 @@ class LoginController
     {
         echo "Desde Login";
     }
+    public static function logout()
+    {
+        echo "Desde Logout";
+    }
+    public static function contraseña()
+    {
+        echo "Desde Contraseña";
+    }
+    public static function recuperar()
+    {
+        echo "Desde Recuperar";
+    }
+    public static function crear()
+    {
+        echo "Desde Crear";
+    }
 }
--- public/index.php
+++ public/index.php
@@ -10,6 +10,18 @@ $router = new Router();
 //Iniciar Sesión
 $router->get('/', [LoginController::class, 'login']);
 $router->post('/', [LoginController::class, 'login']);
+$router->get('/', [LoginController::class, 'logout']);
+
+//Recuperación Password
+$router->get('/contraseña', [LoginController::class, 'contraseña']);
+$router->post('/contraseña', [LoginController::class, 'contraseña']);
+$router->get('/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/recuperar', [LoginController::class, 'recuperar']);
+
+//Crear cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
+
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
