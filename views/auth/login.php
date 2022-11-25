f6cf34963e721bedf52be8a500fe741ad6628361
Moyrasc <noemisolac@gmail.com>
2022-11-25 20:08:22 +0100

    add style form

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,21 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Introduce tu email" />
+    </div>
+    <div class="campo">
+        <laber for="password">Contraseña</laber>
+        <input type="password" id="password" placeholder="Introduce contraseña" name="password" />
+    </div>
+
+    <input type="submit" class="boton" value="Inicio Sesión" />
+
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes cuenta? Pincha aquí</a>
+    <a href="/contraseña">¿Olvidaste tu contraseña?</a>
+</div>
